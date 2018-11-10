71a8685fdb68109e9322fe8c984446fb686e7a82
Joshua Nelson <jyn514@gmail.com>
2018-11-10 13:21:16 -0500

    automated commit adding file 'reverse.c'

---
+++ assets/reverse.c
@@ -0,0 +1,22 @@
+// string literals:
+// L!
+// F
+// c
+
+// impl+167
+void error () {
+  setTerminalRed();
+
+}
+
+bool compare(string, self) {
+  string literal = "3PI";
+  // Kali_Share_password__Password_impl
+  add(literal, __Kali_Share_password_strLit1);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc <= 1) return 0;
+  if (argc > 1) throw exception();
+  if (!compare(string(argv[1]), password)) error();
+}
